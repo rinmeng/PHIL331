bd3433c92a64c468e1a908a964eaddd70f86e3c2
rin-windows <phurinmeng@gmail.com>
2025-03-29 03:22:23 -0700

    refactor Home component to improve card layout and update consent language in description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,14 +25,15 @@ const Home = () => {
   return (
     <div className="min-h-screen p-4">
       <div className="flex flex-col items-center container mx-auto space-y-8">
-        <Card className="w-1/2">
+        <Card className="w-full lg:w-1/2">
           <CardHeader className="text-center text-2xl">
             <CardTitle>Philosophy Assessment</CardTitle>
-            <CardDescription>
+            <CardDescription className="flex">
               This test is designed to assess your philosophical beliefs and
               values. Please answer the questions honestly and to the best of
-              your ability. Your responses will be anonymous and used for
-              research purposes only.
+              your ability. By answering or submitting these questions, you
+              confirm that your responses will be recorded and you consent to
+              the use of this data for research purposes.
             </CardDescription>
           </CardHeader>
         </Card>
